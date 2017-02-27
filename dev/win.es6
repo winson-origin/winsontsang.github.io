76254b287ec71a2f2c05381c33e1a39830e37d02
Winson Tsang <winson.tsang@origin-broadband.co.uk>
2017-02-27 11:18:31 +0000

    Service worker was loading the wrong url.

--- dev/win.es6
+++ dev/win.es6
@@ -3,7 +3,7 @@ const env = {
   path: `https://cdn.rawgit.com/winsontsang/winsontsang.github.io/${env.path}/master/`
 }
 
-navigator.serviceWorker.register(`${path}/src/worker.js`).then( reg => {
+navigator.serviceWorker.register(`${env.path}/src/worker.js`).then( reg => {
   console.log('◕‿◕', reg);
 }, err => {
   console.log('ಠ_ಠ', err);
--- src/win.js
+++ src/win.js
@@ -5,7 +5,7 @@ var env = {
   path: 'https://cdn.rawgit.com/winsontsang/winsontsang.github.io/' + env.path + '/master/'
 };
 
-navigator.serviceWorker.register(path + '/src/worker.js').then(function (reg) {
+navigator.serviceWorker.register(env.path + '/src/worker.js').then(function (reg) {
   console.log('◕‿◕', reg);
 }, function (err) {
   console.log('ಠ_ಠ', err);
