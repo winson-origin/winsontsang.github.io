5488eb0492a68adc26d1d845d49eaca4f4ee1509
Winson Tsang <winson.tsang@origin-broadband.co.uk>
2017-02-27 12:58:24 +0000

    Updated sw.

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith( caches.match(event.request)
     .then(function(response) {
       return response || fetchOffline();
-    });
+    })
   );
 });
 
