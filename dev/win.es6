0e0be84f4a741925f858cdacd01f22b325d8fc4b
Winson Tsang <winson.tsang@origin-broadband.co.uk>
2017-02-27 12:16:45 +0000

    Fixing service worker registration.

--- dev/win.es6
+++ dev/win.es6
@@ -1,9 +1,3 @@
 const env = {
   /** DEFAULT **/
 }
-
-navigator.serviceWorker.register(`/src/worker.js`).then( reg => {
-  console.log('◕‿◕', reg);
-}, err => {
-  console.log('ಠ_ಠ', err);
-});
--- src/win.js
+++ src/win.js
@@ -1,11 +1,5 @@
-'use strict';
+"use strict";
 
 var env = {
   /** DEFAULT **/
 };
-
-navigator.serviceWorker.register('/src/worker.js').then(function (reg) {
-  console.log('◕‿◕', reg);
-}, function (err) {
-  console.log('ಠ_ಠ', err);
-});
